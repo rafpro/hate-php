90d21aebf8f7c94e5a9183e9a21bad06243293cd
Rafiqul Islam <rafique.ngn@gmail.com>
2018-01-24 22:39:30 +0600

    fixed the script

--- app.php
+++ app.php
@@ -3,6 +3,7 @@ require 'vendor/autoload.php';
 
 use Intervention\Image\ImageManagerStatic as Image;
 
+Image::configure(['driver' => 'gd']);
 
 $background = Image::make('./certificate-blank-exact.png');
 
@@ -11,13 +12,13 @@ $profile_picture = Image::make('./avatar.jpg')->resize(220, 220)
 $background->insert($profile_picture, 'top-left', 70, 140);
 
 $background->text('Guddu', 380, 160, function ($font) {
-    $font-size(24);
+    $font->size(24);
     $font->file('./OpenSans-Regular.ttf');
     $font->color('#000000');
 });
 
 $background->text('গার্লফ্রেন্ড', 380, 220, function ($font) {
-    $font-size(24);
+    $font->size(24);
     $font->file('./kalpurush.ttf');
     $font->color('#ec1c24');
 });
